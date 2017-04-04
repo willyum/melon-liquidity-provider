5b0b297d0dab37c7a9d9f48c8f24cb4e3b496359
Will King <iamwillking@gmail.com>
2017-04-04 16:14:49 -0400

    cancel old orders before submitting new orders

--- liquidity.js
+++ liquidity.js
@@ -1,4 +1,6 @@
 const Web3 = require('web3');
+const _= require('underscore');
+
 const contractAddress = '0x7c7B4904e37C71845FDC1c75D20FF6ee3c80eCB4';
 const abiArray = [{"constant":false,"inputs":[{"name":"sell_how_much","type":"uint256"},{"name":"sell_which_token","type":"address"},{"name":"buy_how_much","type":"uint256"},{"name":"buy_which_token","type":"address"}],"name":"make","outputs":[{"name":"id","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"}],"name":"cancel","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"lastOfferId","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"getLastOrderId","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"id","type":"uint256"}],"name":"isActive","outputs":[{"name":"active","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"id","type":"uint256"},{"name":"quantity","type":"uint256"}],"name":"take","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"orders","outputs":[{"name":"sell_how_much","type":"uint256"},{"name":"sell_which_token","type":"address"},{"name":"buy_how_much","type":"uint256"},{"name":"buy_which_token","type":"address"},{"name":"owner","type":"address"},{"name":"active","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"id","type":"uint256"}],"name":"getOwner","outputs":[{"name":"owner","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"id","type":"uint256"}],"name":"getOrder","outputs":[{"name":"","type":"uint256"},{"name":"","type":"address"},{"name":"","type":"uint256"},{"name":"","type":"address"}],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"id","type":"uint256"}],"name":"OrderUpdate","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"sell_how_much","type":"uint256"},{"indexed":true,"name":"sell_which_token","type":"address"},{"indexed":false,"name":"buy_how_much","type":"uint256"},{"indexed":true,"name":"buy_which_token","type":"address"}],"name":"Trade","type":"event"}];
 
@@ -10,10 +12,19 @@ var mlnAdd = '0x4DffEA52b0B4b48c71385ae25de41CE6AD0Dd5a7';
 var liveAskIDs = [];
 var liveBidIDs = [];
 
+function batchCancel(list){
+    if list.length != 0 {
+        var results = _.map(list, contract.cancel);
+        list = _.reject(list, function(orderId, index){return results[index]});
+    }
+    return list;
+}
+
 var request = require('request');
 request('https://api.kraken.com/0/public/Ticker?pair=MLNETH', function (error, response, body) {
     if (!error && response.statusCode == 200) {
         var data = JSON.parse(body);
+        console.log(data['result']);
         var ask = data['result']['XMLNXETH']['a'];
         var bid = data['result']['XMLNXETH']['b'];
 
@@ -27,10 +38,13 @@ request('https://api.kraken.com/0/public/Ticker?pair=MLNETH', function (error, r
         console.log(bid);
         console.log([sellETH, ethAdd, buyMLN, mlnAdd]);
 
+        liveAskIDs = batchCancel(liveAskIDs);
+        liveBidIDs = batchCancel(liveBidIDs);
+
         contract.make(sellMLN, mlnAdd, buyETH, ethAdd);
-        // liveAskIDs.push(contract.make(sellMLN, mlnAdd, buyETH, ethAdd));
+        // liveAskIDs.push(getLastOrderId());
         
         contract.make(sellETH, ethAdd, buyMLN, mlnAdd);
-        // liveAskIDs.push(contract.make(sellETH, ethAdd, buyMLN, mlnAdd));
+        // liveAskIDs.push(getLastOrderId());
     }
 });
