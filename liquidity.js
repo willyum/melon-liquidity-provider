fb50e80e23b34577c5fb2df3c3f7d8f57b279648
Will King <iamwillking@gmail.com>
2017-04-04 14:26:51 -0400

    get kraken price feed

--- liquidity.js
+++ liquidity.js
@@ -1,1 +1,7 @@
-    
+var request = require('request');
+request('https://api.kraken.com/0/public/Ticker?pair=MLNETH', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body);
+        console.log(data['result']);
+    }
+});
